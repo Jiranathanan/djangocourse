90d9ef85b8c16e517de7a378393bb0b3f2f1e5f0
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-06-20 22:26:16 +0700

    add other endpoints

--- app/views.py
+++ app/views.py
@@ -1,14 +1,34 @@
 from app.models import Article
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import ( 
+    CreateView,
+    ListView,
+    UpdateView,
+    DeleteView
+)
 
-def home(request):
-    articles = Article.objects.all()
-    return render(request, "app/home.html", {"articles": articles})
+class ArticleListView(ListView):
+    template_name = "app/home.html"
+    model = Article
+    context_object_name = "articles"
 
 class ArticleCreateView(CreateView):
     template_name = "app/article_create.html"
     model = Article
     fields = ["title", "status", "content", "word_count", "twitter_post"]
     success_url = reverse_lazy("home") 
+
+class ArticleUpdateView(UpdateView):
+    template_name = "app/article_update.html"
+    model = Article
+    fields = ["title", "status", "content", "word_count", "twitter_post"]
+    success_url = reverse_lazy("home") 
+    context_object_name = "articles"
+
+class ArticleDeleteView(DeleteView):
+    template_name = "app/article_delete.html"
+    model = Article
+    success_url = reverse_lazy("home") 
+    context_object_name = "articles"
+
